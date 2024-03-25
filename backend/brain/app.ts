8c5baf537c3fce58f1130eaa549964b49d7b0c6d
Lina <114607173+Caroline-Hornhuber@users.noreply.github.com>
2024-03-25 21:28:11 +0100

    adding the backend endpoint for evaluate month

--- backend/brain/app.ts
+++ backend/brain/app.ts
@@ -3,6 +3,7 @@ import {authRouter} from "../routers/auth-router";
 import dotenv from "dotenv";
 import path from "path";
 import {entryRouter} from "../routers/entry-router";
+import {evaluateRouter} from "../routers/evaluate-router";
 
 dotenv.config();
 require('dotenv').config();
@@ -18,6 +19,7 @@ app.use(express.static("frontend"));
 //routers
 app.use("/api/auth",authRouter);
 app.use("/api/entry",entryRouter);
+app.use("/api/evaluate",evaluateRouter);
 
 app.listen(3000, () => {
     console.log("Server is listening at http://localhost:3000");
---
+++ backend/routers/evaluate-router.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import {isAuthenticated} from "../middleware/auth-handlers";
+import {getMonthData} from "../brain/evaluate_repo";
+import {StatusCodes} from "http-status-codes";
+
+export const evaluateRouter = express.Router();
+
+// GET retrieve month data
+evaluateRouter.get("/:date", isAuthenticated, (req, res) => {
+    const { date } = req.params;
+    const currentUser = req.user.username;
+    const monthData = getMonthData(currentUser, date);
+    if (monthData) {
+        return res.status(StatusCodes.OK).json(monthData);
+    } else {
+        return res.status(StatusCodes.NOT_FOUND).json({ error: "Month data not found" });
+    }
+});
